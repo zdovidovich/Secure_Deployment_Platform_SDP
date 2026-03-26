07b1042c50952a1ae40502c8177ef602068a9a7b
zdovidovich <dovidovichzakhar0@gmail.com>
2026-03-26 18:04:43 +0300

    Trying making site

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from libs.temp_files import save_temp_file, create_inventory_temp_file, cleanup_temp_files
 
 app = Flask(__name__)
 
@@ -8,8 +9,18 @@ def index():
 
 @app.route('/deploy', methods=['POST'])
 def deploy():
-    return "В разработке" 
-# проверка загруженных параметров
+    file_path_ssh_key = save_temp_file(request.files.get('ssh_key'), "ssh_key_")
+    file_path_image = save_temp_file(request.files.get('docker_image'), "docker_image_")
+    file_path_inventory = create_inventory_temp_file(request.form, file_path_ssh_key)    
+    for i in request.form:
+       print(i)
+    cleanup_temp_files()
+    return "Nothing yet"
 
+# проверка загруженных параметров (docker image + priv_key, мб еще проверить остальные параметры (хотя некоторые уже проверяются в ansible))
+# сделать inventory файл, добавить сам хост (имя - хэш(айпи:пользователь:время создания, ansible_host, ansible_user, ansible_private_key_file, ansible_port), загрузить priv_key для ssh)
+# указать дополнительные переменные (ssh_port, app_image_path, app_image_name, app_container_name, app_ports, app_volumes?, app_envs?)  
+# начать запускать роли в правильном порядке
+# в прямом эфире показывать вывод терминала?
 if __name__ == '__main__':
   app.run(debug=True, port=8080)
---
+++ app/libs/temp_files.py
@@ -0,0 +1,63 @@
+import os
+import tempfile
+import uuid
+from werkzeug.utils import secure_filename
+import glob
+
+def save_temp_file(file_obj, prefix='upload_'):
+    """
+    Сохраняет загруженный файл во временную папку с уникальным именем.
+    Возвращает путь к файлу.
+    """
+    original_filename = secure_filename(file_obj.filename)
+    
+    unique_name = f"{prefix}{uuid.uuid4().hex}_{original_filename}"
+    
+    temp_dir = tempfile.gettempdir() 
+
+    file_path = os.path.join(temp_dir, unique_name)
+    
+    file_obj.save(file_path)
+    
+    os.chmod(file_path, 0o600)
+    
+    return file_path
+
+def create_inventory_temp_file(ansible_params: dict, ssh_key_path) -> str:
+    """
+    Создаёт временный inventory файл для Ansible.
+    Возвращает полный путь к файлу.
+    """
+    name = uuid.uuid4().hex[:8]  
+    unique_name = f"inventory_{name}"
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    inventory_dir = os.path.join(base_dir, '..', '..',  'ansible', 'inventory')
+    
+    os.makedirs(inventory_dir, exist_ok=True)
+    
+    file_path = os.path.join(inventory_dir, unique_name)
+    
+    with open(file_path, 'w') as f:
+        f.write("[server]\n")
+        f.write(
+            f"{name} "
+            f"ansible_host={ansible_params['ansible_host']} "
+            f"ansible_port={ansible_params['ansible_port']} "
+            f"ansible_user={ansible_params['ansible_user']} "
+            f"ansible_ssh_private_key_file={ssh_key_path} "
+        )
+    
+    os.chmod(file_path, 0o600)
+    
+    return file_path
+
+def cleanup_temp_files(prefixes=('ssh_key_', 'docker_image_')):
+    temp_dir = tempfile.gettempdir()
+    for prefix in prefixes:
+        for file_path in glob.glob(os.path.join(temp_dir, f'{prefix}*')):
+            try:
+                os.remove(file_path)
+            except OSError:
+                pass  
+    
