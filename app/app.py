31a5d370a4fea6eee9bd29dfc2fd660b9880703a
zdovidovich <dovidovichzakhar0@gmail.com>
2026-04-01 17:22:18 +0300

    Added redirect from / to /deploy

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from routes.deploy import deploy_bp
 
 app = Flask(__name__)
 
-# Регистрируем Blueprint для деплоя
 app.register_blueprint(deploy_bp)
 
-# Можно добавить другие Blueprint'ы позже (api_bp, auth_bp, etc.)
+@app.route('/')
+def index():
+    return redirect('deploy', 301)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
